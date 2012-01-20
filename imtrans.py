765e5047d8263b36c34f97d484b488da4cd74b00
Joan Creus <joan.creus.c@gmail.com>
2012-01-20 19:21:24 +0100

    Més canvis

--- imtrans.py
+++ imtrans.py
@@ -4,29 +4,6 @@ from PIL import Image
 import simplejson
 
 WIDTH = HEIGHT = 32
-"""	this.canvas.width = this.im.naturalWidth;
-	this.canvas.height = this.im.naturalHeight;
-	this.context.drawImage(this.im,0,0);
-	var chunks = [];
-	// Trossos d'alçada
-	for (var r=0; r<this.canvas.height; r += HEIGHT) {
-		var tmplist = [];
-		for (var h=0; h<this.canvas.width; h += WIDTH) { // Trossos horitzontals
-			var data = this.context.getImageData(h,r,h+WIDTH,r+HEIGHT).data;
-			var sum = 0;
-			var rg = 0;
-			var g = 0;
-			var b = 0;
-			for (var i=0; i<data.length; i += 4) {
-				rg += data[i];
-				g += data[i+1];
-				b += data[i+2];
-				sum += 1;
-			}
-			tmplist[tmplist.length] = [Math.round(rg/sum),Math.round(g/sum),Math.round(b/sum)];
-		}
-		chunks[chunks.length] = tmplist;
-	}"""
 
 class ImTrans:
     def __init__(self, im):
@@ -58,4 +35,4 @@ class ImTrans:
 	print "imH = ",he,";"
 
 if __name__ == '__main__':
-    ImTrans("../RomaMedieval/DSC04206.JPG").transform()
+    ImTrans("../RomaMedieval/DSC04105.JPG").transform()
